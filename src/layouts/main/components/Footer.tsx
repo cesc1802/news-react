340c80708445891ff6364dc11bef1742f2727c5e
Cesc Nguyen <thuocnv1802@gmail.com>
2022-01-04 11:12:42 +0700

    feat: footer finished

--- src/layouts/main/components/Footer.tsx
+++ src/layouts/main/components/Footer.tsx
@@ -1,7 +1,87 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-  return <div>This is Footer</div>;
+  return (
+    <div className="">
+      <div className="footer flex p-10 justify-around border-b-2 border-b-sky-800">
+        <div className="flex flex-col items-center">
+          <img
+            className="w-[160px] h-[160px] mb-14"
+            src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2020/12/tgphanoi-150x150.png"
+            alt=""
+          />
+          <div className="text-white text-2xl hover:text-orange-400">
+            Giáo Xứ Hà Hồi
+          </div>
+        </div>
+        <div className="text-white">
+          <div className="text-white text-2xl mb-10 hover:text-orange-400">
+            Trang Liên Kết
+          </div>
+
+          <div className=" hover:text-orange-400">Vatican</div>
+          <div className="hover:text-orange-400">Vatican news</div>
+          <div className="hover:text-orange-400">
+            Hội Đồng Giám Mục Việt Nam
+          </div>
+          <div className="hover:text-orange-400">
+            Ủy ban BAXH-Caritas Việt Nam
+          </div>
+          <div className="hover:text-orange-400">Ủy ban Giáo lý Đức tin</div>
+          <div className="hover:text-orange-400">Ủy ban Kinh Thánh</div>
+          <div className="hover:text-orange-400">Ủy ban Mục vụ Di dân</div>
+          <div className="hover:text-orange-400">
+            Liên hiệp Bề trên Thượng cấp
+          </div>
+        </div>
+
+        <div className="text-white">
+          <div className="hover:text-orange-400">Giáo phận Bà Rịa</div>
+          <div className="hover:text-orange-400">Giáo phận Ban Mê Thuột</div>
+          <div className="hover:text-orange-400">Giáo phận Bắc Ninh</div>
+          <div className="hover:text-orange-400">Giáo phận Bùi Chu</div>
+          <div className="hover:text-orange-400">Giáo phận Cần Thơ</div>
+          <div className="hover:text-orange-400">Giáo phận Đà Lạt</div>
+          <div className="hover:text-orange-400">Giáo phận Đà Nẵng</div>
+          <div className="hover:text-orange-400">Tổng Giáo phận Hà Nội</div>
+          <div className="hover:text-orange-400">Giáo phận Hải Phòng</div>
+          <div className="hover:text-orange-400">Giáo phận Hà Tĩnh</div>
+          <div className="hover:text-orange-400">Tổng Giáo phận Huế</div>
+          <div className="hover:text-orange-400">Giáo phận Hưng Hoá</div>
+          <div className="hover:text-orange-400">Giáo phận Kon Tum</div>
+          <div className="hover:text-orange-400">Giáo phận Lạng Sơn</div>
+          <div className="hover:text-orange-400">Giáo phận Long Xuyên</div>
+          <div className="hover:text-orange-400">Giáo phận Mỹ Tho</div>
+          <div className="hover:text-orange-400">Giáo phận Nha Trang</div>
+        </div>
+
+        <div className="text-white">
+          <div className="hover:text-orange-400">Giáo phận Phan Thiết</div>
+          <div className="hover:text-orange-400">Giáo phận Phát Diệm</div>
+          <div className="hover:text-orange-400">Giáo phận Phú Cường</div>
+          <div className="hover:text-orange-400">Giáo phận Qui Nhơn</div>
+          <div className="hover:text-orange-400">Giáo phận Thái Bình</div>
+          <div className="hover:text-orange-400">Giáo phận Thanh Hoá</div>
+          <div className="hover:text-orange-400">
+            Tổng Giáo phận Sài Gòn – TPHCM
+          </div>
+          <div className="hover:text-orange-400">Giáo phận Vinh</div>
+          <div className="hover:text-orange-400">Giáo phận Vĩnh Long</div>
+          <div className="hover:text-orange-400">Giáo phận Xuân Lộc</div>
+
+          <div className="hover:text-orange-400">Giới trẻ Tổng Giáo phận</div>
+          <div className="hover:text-orange-400">Giáo xứ Đồng Trì</div>
+          <div className="hover:text-orange-400">Giáo xứ Thái Hà</div>
+          <div className="hover:text-orange-400">TTHH Bằng Sở</div>
+          <div className="hover:text-orange-400">TTHH Sở Kiện</div>
+          <div className="hover:text-orange-400">Sinh Viên TGPHN</div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center w-full h-28 text-white text-xl bg-amber-400 text-center">
+        <a>Copyright © 2022 Giáo Xứ Hà Hồi | Powered by Giáo Xứ Hà Hồi</a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
